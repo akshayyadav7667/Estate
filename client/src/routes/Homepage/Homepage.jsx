3ed30becd18f23bdcc7448ba020b6cf17d8fee82
Akshay Kumar <akshaykumar880466@gmail.com>
2025-12-26 17:59:27 +0530

    searchbar toggle update

--- client/src/routes/Homepage/Homepage.jsx
+++ client/src/routes/Homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./Homepage.scss";
 import bg from "../../assets/bg.png";
+import SearchBar from "../../components/searchBar/SearchBar";
 
 export default function Homepage() {
   return (
@@ -14,6 +15,23 @@ export default function Homepage() {
             business, or investment. It plays an important role in providing
             homes, creating jobs, and supporting economic growth.
           </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Year of Experience</h2>
+            </div>
+
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gain</h2>
+            </div>
+
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready </h2>
+            </div>
+          </div>
         </din>
       </div>
 
