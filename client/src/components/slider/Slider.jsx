976d807304c4cc992e0071ddbcdec7f8b9c84e6e
Akshay Kumar <akshaykumar880466@gmail.com>
2026-01-04 02:24:04 +0530

    singlePage

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-import './Slider.scss'
+import React, { useState } from "react";
+import "./Slider.scss";
+import arrow from "../../assets/arrow.png";
 
+export default function Slider({ images }) {
+  const [imageIndex, setImageIndex] = useState(null);
 
-export default function Slider() {
   return (
-    <div>Slider</div>
-  )
+    <div className="slider">
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow">
+            <img src={arrow} />
+          </div>
+          <div className="imgContainer">
+            <img src={images[0]} />
+          </div>
+          <div className="arrow">
+            <img className="right" src={arrow} />
+          </div>
+          <div className="close">X</div>
+        </div>
+      )}
+
+      <div className="bigImage">
+        <img src={images[0]} />
+      </div>
+
+      <div className="smallImages">
+        {images.slice(1).map((item, index) => (
+          <img key={index} src={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
