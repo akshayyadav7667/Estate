11b0c7943a242351c2143123cbe4e01d29acff65
Akshay Kumar <akshaykumar880466@gmail.com>
2026-02-02 23:45:40 +0530

    complete single page

--- client/src/routes/singlePage/SinglePage.jsx
+++ client/src/routes/singlePage/SinglePage.jsx
@@ -4,6 +4,23 @@ import Slider from "../../components/slider/Slider";
 import { singlePostData } from "../../lib/dummy";
 import { userData } from "../../lib/dummy";
 import pin from "../../assets/pin.png";
+import Maps from "../../components/map/Maps";
+import utility from "../../assets/utility.png";
+import fees from "../../assets/fee.png";
+import pet from "../../assets/pet.png";
+import size from "../../assets/size.png";
+import bed from "../../assets/bed.png";
+import restaurant from "../../assets/size.png";
+import school from "../../assets/school.png";
+
+import bath from "../../assets/bath.png";
+
+import bus from "../../assets/bus.png";
+import chat from "../../assets/chat.png";
+import save from "../../assets/save.png";
+
+// import search , SVGAElement, logo, bus, bath
+// import Maps from '../../components/map/Maps'
 
 export default function SinglePage() {
   return (
@@ -37,7 +54,102 @@ export default function SinglePage() {
         </div>
       </div>
       <div className="features">
-        <div className="wrapper">right</div>
+        <div className="wrapper">
+          <p className="title">
+            <p>General</p>
+            <div className="listVertical">
+              <div className="feature">
+                <img src={utility} />
+                <div>
+                  <span>Uitilities</span>
+                  <p>Renter is responsible</p>
+                </div>
+              </div>
+
+              <div className="feature">
+                <img src={pet} />
+                <div>
+                  <span>Pet Policy</span>
+                  <p>Pets Allowed</p>
+                </div>
+              </div>
+
+              <div className="feature">
+                <img src={fees} />
+                <div>
+                  <span>Property Fees</span>
+                  <p>Must have 3x the rent in total household income</p>
+                </div>
+              </div>
+            </div>
+          </p>
+          <p className="title">
+            <p>Sizes</p>
+
+            <div className="sizes">
+              <div className="size">
+                <img src={size} />
+                <span>80sqft</span>
+              </div>
+
+              <div className="size">
+                <img src={bed} />
+                <span>2 bed</span>
+              </div>
+
+              <div className="size">
+                <img src={bath} />
+                <span>1 bathroom</span>
+              </div>
+            </div>
+          </p>
+
+          <p className="title">
+            <p>Nearby Places</p>
+            <div className="listHorizontal">
+              <div className="feature">
+                <img src={school} />
+
+                <div>
+                  <span>School</span>
+                  <p>250m away</p>
+                </div>
+              </div>
+
+              <div className="feature">
+                <img src={bus} />
+
+                <div>
+                  <span>Bus stop</span>
+                  <p>100m away</p>
+                </div>
+              </div>
+
+              <div className="feature">
+                <img src={restaurant} />
+
+                <div>
+                  <span>Resturent</span>
+                  <p>200m away</p>
+                </div>
+              </div>
+            </div>
+          </p>
+          <p className="title">Location</p>
+          <div className="mapContainer">
+            <Maps items={[singlePostData]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src={chat} />
+              Send a Message
+            </button>
+            <button>
+              <img src={save} />
+              Save the Place
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
